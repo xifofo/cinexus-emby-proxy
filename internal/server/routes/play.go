fe6779857204de249e3fedc1d3b17fb16c51b63b
Xifo Wu <xifo.wu@gmail.com>
2025-06-03 15:45:21 +0800

    feat: 支持 STRM 内容是 AList 链接的 302

--- internal/server/routes/play.go
+++ internal/server/routes/play.go
@@ -51,6 +51,11 @@ func ProxyPlay(c echo.Context, proxy *httputil.ReverseProxy, cfg *config.Config,
 		}
 	}
 
+	// 判断 embyPlayPath 是否是 alist url，如果是进行代理
+	if strings.HasPrefix(embyPlayPath, cfg.Alist.URL) {
+		return GetAlistRedirectURL(embyPlayPath, log, cfg, originalHeaders)
+	}
+
 	// 匹配 embyPlayPath 是否在 cfg.Proxy.Paths 中，如果存在，则替换为 cfg.Proxy.Paths 中的 new
 	// 不存在 old 开头的说明不需要代理
 	needProxy := false
@@ -68,27 +73,29 @@ func ProxyPlay(c echo.Context, proxy *httputil.ReverseProxy, cfg *config.Config,
 
 	// userAgent := strings.ToLower(c.Request().Header.Get("User-Agent"))
 	if cfg.Proxy.Method == "alist" {
-		// TODO infuse 不知道还是不是和以前一样
-
-		alistUrl := fmt.Sprintf("%s/d%s", cfg.Alist.URL, embyPlayPath)
-		if cfg.Alist.Sign {
-			alistUrl = fmt.Sprintf("%s?sign=%s", alistUrl, alist.Sign(embyPlayPath, 0, cfg.Alist.APIKey))
-		}
-
-		log.Infof("【ALIST PROXY】Request URI: %s", alistUrl)
-		redirectURL, err := alist.GetRedirectURL(alistUrl, originalHeaders)
-		if err != nil {
-			log.Errorf("获取 Alist 重定向 URL 错误: %v", err)
-			return "", true
-		}
-
-		return redirectURL, false
-	}
-
-	if cfg.Proxy.Method == "115open" {
-
+		return GetAlistRedirectURL(embyPlayPath, log, cfg, originalHeaders)
 	}
 
 	log.Warnln("不支持的代理方法")
 	return "", true
+}
+
+// 通过 Alist 链接直接获取 302 重定向地址
+func GetAlistRedirectURL(alistPath string, log *logger.Logger, cfg *config.Config, originalHeaders map[string]string) (string, bool) {
+	alistUrl := fmt.Sprintf("%s/d%s", cfg.Alist.URL, alistPath)
+	if strings.HasPrefix(alistPath, cfg.Alist.URL) {
+		alistUrl = alistPath
+	}
+
+	if cfg.Alist.Sign {
+		alistUrl = fmt.Sprintf("%s?sign=%s", alistUrl, alist.Sign(alistPath, 0, cfg.Alist.APIKey))
+	}
+
+	redirectURL, err := alist.GetRedirectURL(alistUrl, originalHeaders)
+	if err != nil {
+		log.Errorf("获取 Alist 重定向 URL 错误: %v", err)
+		return "", true
+	}
+
+	return redirectURL, false
 }
